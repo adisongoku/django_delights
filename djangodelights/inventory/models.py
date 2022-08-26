048842ad973536eca298f819447ff9496a078bc6
adisongoku <adrian.wiktorowicz440@gmail.com>
2022-08-26 16:30:06 +0200

    adding models

--- djangodelights/inventory/models.py
+++ djangodelights/inventory/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    available_quantity = models.IntegerField(max_length=20, default=0, on_delete=models.CASCADE)
+    price_per_unit = models.FloatField(max_length=20, default=0.0, on_delete=models.CASCADE)
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField(max_length=20, default=0.0, on_delete=models.CASCADE)
+    
