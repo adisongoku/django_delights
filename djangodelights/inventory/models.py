c03f2fe4c2f087d21968ad3671eb394f0d8100d3
adisongoku <adrian.wiktorowicz440@gmail.com>
2022-08-29 12:19:27 +0200

    corrected models and admin changes

--- djangodelights/inventory/models.py
+++ djangodelights/inventory/models.py
@@ -16,20 +16,23 @@ class Ingredient(models.Model):
     )
 
     name = models.CharField(max_length=255)
-    quantity = models.IntegerField(max_length=20, default=0)
+    quantity = models.IntegerField(default=0)
     unit = models.CharField(max_length=20, choices=WEIGHT_UNITS, default=GRAM)
-    punit_price = models.FloatField(max_length=20, default=0.0)
+    unit_price = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.name
         
 
 class MenuItem(models.Model):
     title = models.CharField(max_length=255)
-    price = models.FloatField(max_length=20, default=0.0)
+    price = models.FloatField(default=0.0)
 
 
 class RecipeRequirement(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    quantity = models.FloatField(max_length=20, default=0.0)
+    quantity = models.FloatField(default=0.0)
 
      
 class Purchase(models.Model):
