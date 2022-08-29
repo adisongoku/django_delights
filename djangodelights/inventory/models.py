4ecc0fafe2470a39e202d3ab4a071f3ee1d222d0
adisongoku <adrian.wiktorowicz440@gmail.com>
2022-08-29 12:02:34 +0200

    created all neccessery models

--- djangodelights/inventory/models.py
+++ djangodelights/inventory/models.py
@@ -3,12 +3,35 @@ from django.db import models
 # Create your models here.
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=255)
-    available_quantity = models.IntegerField(max_length=20, default=0, on_delete=models.CASCADE)
-    price_per_unit = models.FloatField(max_length=20, default=0.0, on_delete=models.CASCADE)
+    KILOGRAM = 'KG'
+    GRAM = 'G'
+    POUND = "LB"
+    OUNCE = "OZ"
 
+    WEIGHT_UNITS = (
+        (KILOGRAM, "Kilogram"),
+        (GRAM, "Gram"),
+        (POUND, "Pound"),
+        (OUNCE, "Ounce"),
+    )
+
+    name = models.CharField(max_length=255)
+    quantity = models.IntegerField(max_length=20, default=0)
+    unit = models.CharField(max_length=20, choices=WEIGHT_UNITS, default=GRAM)
+    punit_price = models.FloatField(max_length=20, default=0.0)
+        
 
 class MenuItem(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.FloatField(max_length=20, default=0.0, on_delete=models.CASCADE)
-    
+    title = models.CharField(max_length=255)
+    price = models.FloatField(max_length=20, default=0.0)
+
+
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField(max_length=20, default=0.0)
+
+     
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
