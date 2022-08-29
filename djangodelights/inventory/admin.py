2141e209ab42bf9a707a494cac78e5e805320727
adisongoku <adrian.wiktorowicz440@gmail.com>
2022-08-29 12:20:05 +0200

    admin change

--- djangodelights/inventory/admin.py
+++ djangodelights/inventory/admin.py
@@ -1,3 +1,20 @@
+from ast import In
 from django.contrib import admin
-
+from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
 # Register your models here.
+
+@admin.site.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'quantity', 'unit', 'unit_price']
+
+@admin.site.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price']
+
+@admin.site.register(RecipeRequirement)
+class RecipeRequirementAdmin(admin.ModelAdmin):
+    list_display = ['menu_item', 'ingredient', 'quantity']
+
+@admin.site.register(Purchase)
+class Purchase(admin.ModelAdmin):
+    list_display = ['menut_item', 'timestamp']
